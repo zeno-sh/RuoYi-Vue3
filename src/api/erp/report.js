3f9e9d767dfb9704a17e9b2067f4e10cb887d80f
zeno <zhulf99663@126.com>
2024-02-16 22:20:04 +0800

    增加首页销量卡片

--- src/api/erp/report.js
+++ src/api/erp/report.js
@@ -1,10 +1,18 @@
-import request from '@/utils/request'
+import request from "@/utils/request";
 
 // 查询业绩报表
 export function listReport(query) {
-    return request({
-      url: '/erp/report/list',
-      method: 'get',
-      params: query
-    })
-  }
+  return request({
+    url: "/erp/report/list",
+    method: "get",
+    params: query,
+  });
+}
+
+export function volumn(query) {
+  return request({
+    url: "/erp/report/volume/product",
+    method: "get",
+    params: query,
+  });
+}
